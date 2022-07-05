9edc4056fd4002b25cc6fe804e0a109f9042d5dd
Boris Kuchin <bvkuchin@gmail.com>
2022-07-05 21:56:24 +0300

    server added 1 thread for the logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,23 +5,35 @@ import java.net.Socket;
 public class Main {
 
     private static final int PORT = 8186;
+    private static Socket socket;
+    private static DataOutputStream outputStream;
+    private static DataInputStream inputStream;
 
     public static void main(String[] args) {
-        Socket socket = null;
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Сервер запущен! Ожидаем сообщений");
-            socket = serverSocket.accept();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        Thread t = new Thread(() -> {
+            while (true) {
+                try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                    System.out.println("Сервер запущен! Ожидаем соединения");
+                    socket = serverSocket.accept();
+                    System.out.println("Подключение установлено");
+                    outputStream = new DataOutputStream(socket.getOutputStream());
+                    inputStream = new DataInputStream(socket.getInputStream());
 
-            while (reader.ready()) {
-                System.out.println(reader.readLine());
+                    while (true) {
+                        String message = inputStream.readUTF();
+                        System.out.println(message);
+                        outputStream.writeUTF("Ответ сервера: \n" + message);
+                        outputStream.flush();
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+        });
+        t.start();
 
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
