ff065b93d142634cd982d57f022beebfae029c15
Boris Kuchin <bvkuchin@gmail.com>
2022-07-06 01:29:39 +0300

    responce to client and console input done. reconnection error left

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.TableHeaderUI;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,30 +10,72 @@ public class Main {
     private static DataOutputStream outputStream;
     private static DataInputStream inputStream;
 
+    private static boolean isConnected = false;
+
     public static void main(String[] args) {
-        Thread t = new Thread(() -> {
+
+        Thread receivingMessageFromClientThread = new Thread(() -> {
             while (true) {
-                try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                    System.out.println("Сервер запущен! Ожидаем соединения");
-                    socket = serverSocket.accept();
-                    System.out.println("Подключение установлено");
-                    outputStream = new DataOutputStream(socket.getOutputStream());
-                    inputStream = new DataInputStream(socket.getInputStream());
 
-                    while (true) {
-                        String message = inputStream.readUTF();
-                        System.out.println(message);
-                        outputStream.writeUTF("Ответ сервера: \n" + message);
-                        outputStream.flush();
-                    }
-
-
+                try {
+                    String message = inputStream.readUTF();
+                    System.out.println(message);
+                    outputStream.writeUTF("Эхо-ответ сервера: \n" + message);
+                    outputStream.flush();
                 } catch (IOException e) {
                     e.printStackTrace();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+            }
+
+        });
+        Thread gettingMessageFromConsoleThread = new Thread(() -> {
+            while (true) {
+                try {
+                    String consoleInput = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                    outputStream.writeUTF("Напечатано с консоли сервера: \n" + consoleInput);
+                    outputStream.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
-        t.start();
+
+
+
+        Thread connectionThread = new Thread(() -> {
+            while (true) {
+
+                while (socket == null) {
+                    try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                        System.out.println("Сервер запущен! Ожидаем соединения");
+                        socket = serverSocket.accept();
+                        System.out.println("Подключение установлено");
+                        outputStream = new DataOutputStream(socket.getOutputStream());
+                        inputStream = new DataInputStream(socket.getInputStream());
+                        gettingMessageFromConsoleThread.start();
+                        receivingMessageFromClientThread.start();
+                    } catch (EOFException e) {
+                        e.printStackTrace();
+                        socket = null;
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+
+        });
+
+        connectionThread.start();
+
+
+
+
 
 
     }
